bca15086cf571aff8821e6807be10bcd73e23cbe
yuulye <yuulye@gmail.com>
2020-10-05 02:48:45 +0700

    npm i: express, colors

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('index.js');
+require('colors');
+console.log(
+  `>>> App started at ${(new Date()).toTimeString()}`
+  .cyan.underline.bold
+);
+console.log('loading core/web...');
+require('./core/web');
+console.log('...core/web loaded!');
+
